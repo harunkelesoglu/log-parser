0c77d9b23224c8ab03233fb8296c84502d478330
harunkelesoglu <harunkelesoglu_@hotmail.com>
2023-07-09 15:52:50 +0300

    feat: added cli feature - Added `commander` dev dependecy to be able to handle cli options - Changed rootDir in tsconfig because of resolution error of package.json - Installed ts-node dev dependecy to be able to debug

---
+++ src/cli.ts
@@ -0,0 +1,12 @@
+import { Command } from 'commander';
+import * as pkg from '../package.json';
+
+const program = new Command();
+program
+    .name(pkg.name)
+    .description(pkg.description)
+    .version(pkg.version)
+    .requiredOption('--input <inputFile>', 'Log file path that will be parsed. The extension should be *.log')
+    .requiredOption('--output <outputFile>', 'Output file path that will be written');
+
+export default program;
